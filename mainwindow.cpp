46a1804f0cae3433eeb88aa2c10e4ae7058e8199
Эльдар_Шерзодович Рахимов <reash24ki259@2-3-06-14-alt.kf.bmstu.local>
2025-03-31 12:51:18 +0300

    Добавлена реализация кнопок.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QRandomGenerator>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
+    connect(ui->generateButton, &QPushButton::clicked, this, &MainWindow::onGenerateClicked);
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +18,22 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::onGenerateClicked()
+{
+    bool okMin, okMax;
+    int min = ui->minEdit->text().toInt(&okMin);
+    int max = ui->maxEdit->text().toInt(&okMax);
+
+    if(!okMin || !okMax) {
+        ui->resultLabel->setText("<font color='red'>Введите числа!</font>");
+        return;
+    }
+
+    if(min >= max) {
+        ui->resultLabel->setText("<font color='red'>Min должен быть меньше Max!</font>");
+        return;
+    }
+
+    int randomNumber = QRandomGenerator::global()->bounded(min, max + 1);
+    ui->resultLabel->setText(QString::number(randomNumber));
+}
